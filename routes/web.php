ac4204e5492dbe8a254484780384c915566378f3
rishu <rishusingh5943@gmail.com>
2025-02-28 15:16:22 +0530

    routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[HomeController::class,'Home']);
+Route::get('/',[HomeController::class,'Home'])->name('home');
 
 Route::get('/contact',function(){
     return view('pages.contact');
